61e36bc7e2fb80903eb2bac2afbe8094faaf5996
atebha <ahsantebha@outlook.com>
2024-11-26 12:03:17 -0800

    Create time travel function

--- custom_module.py
+++ custom_module.py
@@ -1,1 +1,5 @@
 # Custom Module
+
+# Time Travelling Function
+def generate_time_travel_message(year, destination, cost, currency):
+    return f"Prepare for jump! You are travelling to {destination} in the year {year}. The cost of the trip is {currency}{cost}."
