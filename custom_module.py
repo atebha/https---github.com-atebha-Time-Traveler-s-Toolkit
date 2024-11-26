6155315d9937b9e5564d28f139a3ce53aef21068
atebha <ahsantebha@outlook.com>
2024-11-26 12:08:41 -0800

    Step 2

--- custom_module.py
+++ custom_module.py
@@ -3,3 +3,4 @@
 # Time Travelling Function
 def generate_time_travel_message(year, destination, cost, currency):
     return f"Prepare for jump! You are travelling to {destination} in the year {year}. The cost of the trip is {currency}{cost}."
+
