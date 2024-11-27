f6ceaad535071d88917da12903b067209d7e160a
atebha <ahsantebha@outlook.com>
2024-11-27 11:41:51 -0800

    crete date & time and print response.

--- Time Traveler's Toolkit.py
+++ Time Traveler's Toolkit.py
@@ -1,2 +1,12 @@
 # create initial version of Time Traveler's Toolkit
 
+import custom_module as cm
+import datetime as dt
+import decimal as decimal
+from random import randint
+from random import choice
+
+date = dt.datetime.today()
+time = dt.datetime.now().time()
+
+print(f"Today's date is {date}. The current {time}.")
