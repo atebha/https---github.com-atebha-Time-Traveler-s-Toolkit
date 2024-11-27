41969cb54fedd11127a3423efb892458831db306
atebha <ahsantebha@outlook.com>
2024-11-27 13:24:30 -0800

    buiiling abs time cost calc

--- Time Traveler's Toolkit.py
+++ Time Traveler's Toolkit.py
@@ -6,7 +6,14 @@ import decimal as decimal
 from random import randint
 from random import choice
 
+# Captue current date and time
 date = dt.datetime.today()
 time = dt.datetime.now().time()
 
 print(f"Today's date is {date}. The current {time}.")
+
+# Calculate cost of time travel
+base_cost = decimal.Decimal("2000.00")
+target_year = input("Enter the target year for time travel: ")
+
+true_cost = abs()
