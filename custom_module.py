f694539413f2a7150b3dadf0978ca5ed6a527958
atebha <ahsantebha@outlook.com>
2024-11-27 11:21:29 -0800

    changed return from import module

--- custom_module.py
+++ custom_module.py
@@ -2,5 +2,5 @@
 
 # Time Travelling Function
 def generate_time_travel_message(year, destination, cost, currency):
-    return f"Prepare for jump! You are travelling to {destination} in the year {year}. The cost of the trip is {currency}{cost}."
+    return f"Prepare for jump! You are travelling to {destination} in the year of {year}. The cost of the trip is {currency}{cost}."
 
